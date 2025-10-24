45cb1839556c118f5a2049eb90e4c2496487e314
Abenezer11Y <abenezerminale7@gmail.com>
2025-10-24 15:10:44 +0300

    Add files via upload

---
+++ THEmain.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+birthday = input("When is your Birthday?")
+print("I have asked Jhon, Paul, Peter and Solomen when their birthday is. \n Now I am gonna write them down along with yours!")
+
+print(name,"'s birthday is on ", birthday)
+print("Paul's birthday is on June 27")
+print("Jhon's birthday is on April 4")
+print("Peter's birthday is on December 25")
+print("And Finnaly, Solomen's birthday is actually ", birthday, ", just like yours!")
